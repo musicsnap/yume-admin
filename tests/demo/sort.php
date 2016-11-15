08896ef0268937f191d5a7d59c0c16dd17f4c6d0
music <musicsnap@163.com>
2016-11-15 17:20:42 +0800

    no message

--- tests/demo/sort.php
+++ tests/demo/sort.php
@@ -5,3 +5,25 @@
  * Date: 2016/11/15
  * Time: 13:16
  */
+
+if(!function_exists('getfile')){
+    function getfile($dir){
+        $files = array();
+
+        if($handles = opendir($dir)){
+                while (($file=readdir($handles))!==false){
+                    if($file != '..' && $file != '.'){
+                        if(is_dir($dir."/".$file))
+                        {
+                            $files[$file]=my_scandir($dir."/".$file);
+                        }
+                        else{
+                            $files[] = $file;
+                        }
+                    }
+                }
+        }
+        closedir($handles);
+        return $files;
+    }
+}
